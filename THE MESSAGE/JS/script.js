f069d0a2302f276a3032340609f23b00abee8165
Kauan <162659253+solidsoon@users.noreply.github.com>
2024-04-29 22:00:01 -0300

    Add files via upload

---
+++ THE MESSAGE/JS/script.js
@@ -0,0 +1,14 @@
+document.querySelector("#message-input").addEventListener("keydown", (event) => {
+  if(event.key == "Enter")
+  getMessage()
+});
+
+document.querySelector("#submit").addEventListener("click", () =>{
+  getMessage()
+});
+
+getMessage = () => {
+  document.querySelector("#message-output").textContent = document.querySelector("#message-input").value;
+  document.querySelector("#message-input").value = "";
+}
+
